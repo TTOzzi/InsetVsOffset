88ca332d1a4dc30e95711a7e93662bec0c566454
TTOzzi <devttozzi@gmail.com>
2020-09-06 15:40:56 +0900

    feat: Offset 슬라이더 추가
    offset 값을 조절하는 슬라이더
    스크롤 뷰가 조절된 offset 값에 따라 움직임

--- InsetVsOffset/InsetVsOffset/ViewController.swift
+++ InsetVsOffset/InsetVsOffset/ViewController.swift
@@ -13,7 +13,8 @@ final class ViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var topOffset: NSLayoutConstraint!
     @IBOutlet weak var bottomOffset: NSLayoutConstraint!
-    
+    @IBOutlet weak var offsetSlider: UISlider!
+    @IBOutlet weak var offsetValueLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +24,14 @@ final class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         updateOffsetViews(scrollView)
+        configureOffsetSlider()
+    }
+    
+    private func configureOffsetSlider() {
+        let offset = scrollView.contentSize.height - scrollView.frame.height
+        offsetSlider.minimumValue = 0
+        offsetSlider.maximumValue = Float(offset)
+        offsetSlider.value = 0
     }
     
     private func updateOffsetViews(_ scrollView: UIScrollView) {
@@ -31,10 +40,18 @@ final class ViewController: UIViewController {
         topOffset.constant = topOffsetConstant >= 0 ? topOffsetConstant : 0
         bottomOffset.constant = bottomOffsetConstant >= 0 ? bottomOffsetConstant : 0
     }
+    
+    @IBAction func offsetSliderValueChanged(_ sender: UISlider) {
+        offsetValueLabel.text = String(Int(sender.value))
+        scrollView.contentOffset = CGPoint(x: 0, y: CGFloat(sender.value))
+        updateOffsetViews(scrollView)
+    }
 }
 
 extension ViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         updateOffsetViews(scrollView)
+        offsetSlider.value = Float(scrollView.contentOffset.y)
+        offsetValueLabel.text = String(Int(scrollView.contentOffset.y))
     }
 }
