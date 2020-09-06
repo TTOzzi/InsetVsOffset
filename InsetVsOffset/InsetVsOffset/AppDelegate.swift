e0e01e291fc82e2b8b8462e0f80c640c91bde74f
TTOzzi <devttozzi@gmail.com>
2020-09-06 14:21:08 +0900

    feat: InsetVsOffset 프로젝트 생성
    프로젝트 생성 및 불필요 코드 제거

---
+++ InsetVsOffset/InsetVsOffset/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  InsetVsOffset
+//
+//  Created by TTOzzi on 2020/09/06.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ InsetVsOffset/InsetVsOffset/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  InsetVsOffset
+//
+//  Created by TTOzzi on 2020/09/06.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
