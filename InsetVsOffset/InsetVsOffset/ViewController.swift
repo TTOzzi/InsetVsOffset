814ca79ab8c29e4a0692238bdcc2d3f8adb30f3f
TTOzzi <devttozzi@gmail.com>
2020-09-06 15:14:38 +0900

    feat: 스크롤 시 Offset 표시
    스크롤 뷰 상단, 하단에 붙인 뷰의 높이를 조절해 Offset 을 표시

--- InsetVsOffset/InsetVsOffset/ViewController.swift
+++ InsetVsOffset/InsetVsOffset/ViewController.swift
@@ -10,8 +10,31 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var topOffset: NSLayoutConstraint!
+    @IBOutlet weak var bottomOffset: NSLayoutConstraint!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.delegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        updateOffsetViews(scrollView)
+    }
+    
+    private func updateOffsetViews(_ scrollView: UIScrollView) {
+        let topOffsetConstant = scrollView.contentOffset.y
+        let bottomOffsetConstant = scrollView.contentSize.height - scrollView.frame.height - scrollView.contentOffset.y
+        topOffset.constant = topOffsetConstant >= 0 ? topOffsetConstant : 0
+        bottomOffset.constant = bottomOffsetConstant >= 0 ? bottomOffsetConstant : 0
     }
 }
 
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        updateOffsetViews(scrollView)
+    }
+}
